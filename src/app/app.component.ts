d63ea351809b636a1d3b66065cb0a2c53185a75f
John Doe <johndoe@example.com>
2018-08-15 19:48:03 -0400

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-}
+  
+  arrayOfAnimals: Array<any> = [
+      {
+        id: 1,
+        category: 'mammal',
+        name: 'Dog'
+      }, {
+        id: 2,
+        category: 'oviparous',
+        name: 'Duck'
+      }, {
+        id: 3,
+        category: 'mammal',
+        name: 'Elephant'
+      }, {
+        id: 4,
+        category: 'reptile',
+        name: 'Snake'
+      }
+    ];
+  }
+
